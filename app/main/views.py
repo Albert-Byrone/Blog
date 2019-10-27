809654248143c64e7a1add6c0b4dcab2d6e84a2c
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-27 18:47:08 +0300

    import the needed modules for the main module

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,13 @@
-from flask import render_template
-from . import main
+from flask import render_template,url_for,request,flash,redirect,abort
+from app.main  import main
+from app.models import User,Blog,Comment,Subscriber
+from .. import db
+from app.requests import get_quotes
+from .forms import UpdateProf,CreateBlog
+from flask_login import login_required,current_user
+import secrets
+import os
+from PIL import Image
 
 @main.route('/')
 def index():
