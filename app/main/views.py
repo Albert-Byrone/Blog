c4cf7efedf9310fc43fc2445e60e7a7de65b2b72
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-27 18:50:51 +0300

    function that saves picture by a random name

--- app/main/views.py
+++ app/main/views.py
@@ -18,3 +18,12 @@ def index():
     return render_template('index.html',quote=quotes,blogs=blogs)
 
 def save_pic(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join('app/static/photos', picture_filename)
+
+    output_size = (80,80)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
