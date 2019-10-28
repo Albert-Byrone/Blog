1cef61c3eae5b3a7deb7693fbca90eac1b5d34d2
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-28 07:05:37 +0300

    add a user model

--- app/models.py
+++ app/models.py
@@ -7,3 +7,17 @@ from datetime import datetime
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+class User(UserMixin,db.Model):
+    
+    __tablename__ = 'users'
+
+    id =db.Column(db.Integer,primary_key =True)
+    username = db.Column(db.String(255),unique=True,nullable=False)
+    email = db.Column(db.String(255),unique=True,nullable=False)
+    bio  = db.Column(db.String(255),default='My default Bio')
+    profile_pic_path = db.Column(db.String(255),default='default.png')
+    hashed_password = db.Column(db.String(255),nullable=False)
+    blog = db.relationship('Blog',backref='user',lazy='dynamic')
+    comment = db.relationship('Comment',backref='user',lazy='dynamic')
+
