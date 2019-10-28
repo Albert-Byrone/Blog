1623b6fc652f23364139740c93f07b648f1efe8d
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-28 12:27:57 +0300

    add email configuration

--- config.py
+++ config.py
@@ -2,6 +2,11 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://byrone:Albert254@localhost/blog'
     SECRET_KEY = 'albert'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
