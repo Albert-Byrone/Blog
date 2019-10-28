1849e839b9350d65b56a2fe3bda97c3d8b8dfe42
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-28 16:35:14 +0300

    update file

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://byrone:Albert254@localhost/blog'
-    SECRET_KEY = 'albert'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://byrone:Albert254@localhost/blog'
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
