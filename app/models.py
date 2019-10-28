e7876958ad41e24f1a27a25f1bc8b2897a1a70d0
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-28 07:05:11 +0300

    function that loads the user with  an id

--- app/models.py
+++ app/models.py
@@ -3,3 +3,7 @@ from flask_login import current_user,UserMixin
 from werkzeug.security import  generate_password_hash,check_password_hash
 from datetime import datetime
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
