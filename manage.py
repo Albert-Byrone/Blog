1a57db17793653c1a2b99a9f867895850a5c8def
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-29 10:21:40 +0300

    update config files

--- manage.py
+++ manage.py
@@ -17,12 +17,12 @@ manager.add_command('run',Server(use_debugger=True))
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User)
-    
+
 @manager.command
 def test():
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == "__main__":
